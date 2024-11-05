d32c3d9a5e5e2006ccec24b7a296992625a2ddd1
AntChong1226 <antoniochong1820@comunidad.unam.mx>
2024-11-05 02:44:00 -0600

    hotfix: build issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['randomuser.me'],
-    },
-  };
+  output: "standalone",
   
-  export default nextConfig;
-  
+};
+
+export default nextConfig;

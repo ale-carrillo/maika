c8295dae703d63de8dfa7c275143c4e9cb880d40
Jhusteene Salgado <jhusteenes@gmail.com>
2024-11-24 17:52:50 -0600

    feat: modifing menu page to create and view meals

---
+++ app/constants/menu/constants.jsx
@@ -0,0 +1,32 @@
+// Constants for menu page.
+export const initialRows = [
+    {
+        id: 1,
+        meal: "Pasta ",
+        description: "type of food made from a mixture of flour, eggs, and water that is formed into different shapes and then boiled",
+        price: 20,
+        image: "/menu/" + "spagetti.jpg"
+    },
+    {
+        id: 2,
+        meal: "Osso",
+        description: "a flat, round piece of bread baked with cheese, tomatoes, and sometimes meat and vegetables",
+        price: 20,
+        image: "/menu/" + "osso.jpg"
+    },
+    {
+        id: 3,
+        meal: "Lasagna ",
+        description: "a food dish that consists of layers of pasta, sauce, and a filling such as meat or cheese, baked in an oven",
+        price: 20,
+        image: "/menu/" + "lasagna.png"
+    },
+    {
+        id: 4,
+        meal: "Caprese",
+        description: "a salad containing lettuce with small pieces of fried bread, mixed with a dressing",
+        price: 20,
+        image: "/menu/" + "caprese.jpg"
+    },
+  ];
+  
